4c7381294d70a0cf0b60b9b6de57de726b48aead
Masud8851 <masud1971.cse@gmail.com>
2021-08-15 04:46:54 +0600

    Baper Bank

--- js/banking.js
+++ js/banking.js
@@ -4,10 +4,24 @@ document.getElementById('deposit-button').addEventListener('click', function(){
     const depositInput = document.getElementById('deposit-input');
     const depositAmount = depositInput.value;
     
-    // pass input deposit to Total Deposit
+    // pass deposit input  to Total Deposit
     const totalDeposit =document.getElementById('deposit-total')
     totalDeposit.innerText = depositAmount;
     
     // clear the input field 
     depositInput.value='';
+})
+
+// handle withdraw button event
+document.getElementById('deposit-button').addEventListener('click', function(){
+    // get withdraw input
+    const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawAmount = withdrawInput.value;
+    
+    // pass withdraw input  to  withdrawTotal 
+    const totalWithdraw = document.getElementById('withdraw-total')
+    totalWithdraw.innerText = withdrawAmount;
+    
+    // clear the input field 
+    withdrawInput.value='';
 })
