cbd571d40f106636c1034599bfc612d78b22822b
Masud8851 <masud1971.cse@gmail.com>
2021-08-15 05:39:22 +0600

    Baper Bank

--- js/banking.js
+++ js/banking.js
@@ -9,22 +9,37 @@ document.getElementById('deposit-button').addEventListener('click', function(){
     const previousTotalDeposit = parseFloat(totalDeposit.innerText);
     const newDepositTotal = previousTotalDeposit + newDepositAmount;
     totalDeposit.innerText = newDepositTotal;
+
+    // update Balance button event
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+    const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+    balanceTotal.innerText = newBalanceTotal;
     
     // clear the input field 
     depositInput.value='';
 })
 
-// withdraw deposit button event
+
+// handle Withdraw button event
 document.getElementById('withdraw-button').addEventListener('click', function(){
     // get withdraw input
     const withdrawInput = document.getElementById('withdraw-input');
     const newWithdrawAmount = parseFloat(withdrawInput.value);
     
-    // pass withdraw input  to Total Withdraw
+    // pass withdraw input  to Total withdraw
     const totalWithdraw =document.getElementById('withdraw-total')
     const previousTotalWithdraw = parseFloat(totalWithdraw.innerText);
     const newWithdrawTotal = previousTotalWithdraw + newWithdrawAmount;
     totalWithdraw.innerText = newWithdrawTotal;
+
+    // update Balance button event
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+    const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+    balanceTotal.innerText = newBalanceTotal;
     
     // clear the input field 
     withdrawInput.value='';
@@ -32,17 +47,5 @@ document.getElementById('withdraw-button').addEventListener('click', function(){
 
 
 
-// handle Balance button event
-document.getElementById('withdraw-button').addEventListener('click', function(){
-    // get withdraw input
-    const withdrawInput = document.getElementById('withdraw-input');
-    const withdrawAmount = withdrawInput.value;
-    
-    // pass withdraw input  to  withdrawTotal 
-    const totalWithdraw = document.getElementById('withdraw-total')
-    totalWithdraw.innerText = withdrawAmount;
-    
-    // clear the input field 
-    withdrawInput.value='';
-})
+
 
