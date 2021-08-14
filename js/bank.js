c5dae50b0ca654714a84b5198ab0cb4e39e293f3
Masud8851 <masud1971.cse@gmail.com>
2021-08-15 03:11:23 +0600

    Baper Bank

--- js/bank.js
+++ js/bank.js
@@ -2,7 +2,9 @@ document.getElementById('login-submit').addEventListener('click',function(){
     // get user email
     const emailField = document.getElementById('user-email');
     const userEmail = emailField.value;
-    // console.log(userEmail);
 
     // get user password
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+    
 });
